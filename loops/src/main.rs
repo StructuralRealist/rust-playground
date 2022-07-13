08b4af1ae43392d6756c9602b7d2aff0169686c3
Leon van der Grient <leon@devtastic.co>
2022-07-13 09:17:47 +0200

    Initial commit

---
+++ loops/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < a.len() {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
